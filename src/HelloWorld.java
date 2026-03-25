6918341e029d1d773b44a611bd01b2559e100e1b
kaali18 <kasinathkv6@gmail.com>
2026-03-25 11:45:56 +0530

    Helloworld printed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
 public class HelloWorld {
-    // TODO: Write your program here to print the required output
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
